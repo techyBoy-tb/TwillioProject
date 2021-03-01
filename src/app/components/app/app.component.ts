8e3194d6de479148eb7e6f3be650de2f662b147e
gareth <gareth.clifford@santander.co.uk>
2021-03-01 21:35:00 +0000

    GC - Finishing off basic functionality

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ApiService } from 'src/app/service/api/api.service';
 import { FormService } from 'src/app/service/form/form.service';
@@ -9,40 +9,42 @@ import { createLocalVideoTrack } from 'twilio-video';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'twilio-project';
-  @ViewChild('preview', { static: false }) previewElement: ElementRef;
+  loading: boolean = true;
+  roomName: string = '';
+  @ViewChild('remoteParticipants', { static: false }) previewElement: ElementRef;
 
   constructor(private apiService: ApiService, public formService: FormService, private render: Renderer2) { }
 
   public roomInfo: FormGroup = this.formService.getRoomInfo();
 
-  ngOnInit(): void {
-    this.initializeCamera();
-  }
-
   async initializeCamera(): Promise<void> {
+    this.loading = !this.loading;
+    this.roomName = this.roomInfo.get('roomName').value.replaceAll(" ", "");
     const videoTrack = await createLocalVideoTrack();
     const videoElement = videoTrack.attach();
-    this.render.setStyle(videoElement, 'height', '640px');
-    this.render.setStyle(videoElement, 'width', '640px');
+    this.render.setStyle(videoElement, 'height', '100%');
+    this.render.setStyle(videoElement, 'width', '100%');
     this.render.appendChild(this.previewElement.nativeElement, videoElement);
   }
 
-  doThis() {
-    const personName = this.roomInfo.get('personName').value.replaceAll(" ", "");
-    const roomName = this.roomInfo.get('roomName').value.replaceAll(" ", "");
+  async connectToRoom() {
+    await this.initializeCamera().then(() => {
+      const personName = this.roomInfo.get('personName').value.replaceAll(" ", "");
+      const roomName = this.roomInfo.get('roomName').value.replaceAll(" ", "");
 
-    const accessToken = this.apiService.getAccessToken(personName, roomName).subscribe(async res => {
-      const token = res.token;
-      this.apiService.joinOrCreateRoom(token.toString()).then(resp => {
+      this.apiService.getAccessToken(personName, roomName).subscribe(async res => {
+        const token = res.token;
+        await this.apiService.joinOrCreateRoom(token.toString()).then(resp => {
+          console.log('this is the response of create join room:', resp);
+        });
       });
-      // this.apiService.createRoom(token.toString());
     });
+  }
 
-
-    // const client = connect(this.accountSid, this.authToken);
-    // connect.viderooms.create({ uniqueName: 'DailyStandup' })
-    // .then(room => o.console.log(room.sid));
+  leaveRoom() {
+    this.loading = !this.loading;
+    this.apiService.leaveRoom();
   }
 }
--- src/app/service/api/api.service.ts
+++ src/app/service/api/api.service.ts
@@ -1,25 +1,20 @@
 import { JsonObject } from '@angular-devkit/core';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import {
   connect,
-
-  createLocalVideoTrack
+  createLocalVideoTrack, Room
 } from 'twilio-video';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  $roomsUpdated: Observable<boolean>;
   tokenUrl = 'https://lemon-dove-8474.twil.io/video-token?identity='
-
-  private roomBroadcast = new ReplaySubject<boolean>();
+  currentRoom: Room;
 
   constructor(private http: HttpClient) {
-    this.$roomsUpdated = this.roomBroadcast.asObservable();
   }
 
   public getAccessToken(personName: string, roomName: string) {
@@ -32,59 +27,53 @@ export class ApiService {
       );
   }
 
+  async leaveRoom() {
+    this.currentRoom.disconnect();
+  }
   async joinOrCreateRoom(token: string) {
-    const localVideoTrack = await createLocalVideoTrack({ width: 640 });
+    const localVideoTrack = await createLocalVideoTrack({ width: 100 });
+
     const room = connect(token, { localVideoTrack, dominantSpeaker: true, }).then(room => {
-      console.log('successfully connected to room: ', room);
+      this.currentRoom = room;
 
-      room.on('participantConnected', participant => {
-        console.log('A new particiapnt has joined: ', participant);
-        participant.tracks.forEach(publication => {
-          if (publication.isSubscribed) {
-            const track = participant.track;
-            document.getElementById('remote-media-container').appendChild(track.attach());
-          }
-        });
-        participant.on('trackSubscribed', track => {
-          document.getElementById('remote-media-container').appendChild(track.attach());
-        });
-      });
+      room.participants.forEach(this.participantConnected);
+      room.on("participantConnected", this.participantConnected);
 
-      room.participants.forEach(participant => {
-        participant.tracks.forEach(publication => {
-          if (publication.track) {
-            document.getElementById('remote-media-container').appendChild(publication.track.attach());
-          }
-        });
-        participant.on('trackSubscribed', track => {
-          document.getElementById('remote-media-container').appendChild(track.attach());
-        });
-      });
-      console.log('this is the parts of the room', room.localParticipant.identity);
-
-
-      // room.once('participantDisconnected', participant => {
-      //   console.log(`Participant "${participant.identity}" has disconnected from the Room`);
-      //   participant.track.stop();
-      //   participant.unpublish();
-      //   document.getElementById('remote-media-container').removeChild(participant.track.detach());
-      // });
-      room.once('participantDisconnected', participant => {
-        console.log(`Participant "${participant.identity}" has disconnected from the Room`);
-        document.getElementById('remote-media-container').removeChild(participant.track.detach());
-
-      });
-      room.on('participantDisconnected', participant => {
-        console.log(`Participant disconnected: ${participant.identity}`);
-      });
-      room.on('disconnected', room => {
-        // Detach the local media elements
-        room.localParticipant.tracks.forEach(publication => {
-          const attachedElements = publication.track.detach();
-          attachedElements.forEach(element => element.remove());
-        });
-      });
-
+      room.on("participantDisconnected", this.participantDisconnected);
+      room.once("disconnected", (error) =>
+        room.participants.forEach(this.participantDisconnected)
+      );
     });
+  }
+
+  participantConnected = (participant) => {
+    console.log(`Participant ${participant.identity} connected.`);
+
+    const parentDiv = document.createElement('div');
+    parentDiv.id = `parent-${participant.sid}`;
+    parentDiv.classList.add("card");
+    const childDiv = `<h3>${participant.identity}</h3><div class="thinggg" id=${participant.sid}></div>`
+    parentDiv.innerHTML = childDiv;
+
+    document.getElementById('card-container').appendChild(parentDiv);
+    participant.on('trackSubscribed', track => this.trackSubscribed(participant.sid, track));
+    participant.on('trackUnsubscribed', this.trackUnsubscribed);
+
+    participant.tracks.forEach(publication => {
+      if (publication.isSubscribed) {
+        this.trackSubscribed(participant.sid, publication.track);
+      }
+    });
+  }
+  participantDisconnected = (participant) => {
+    console.log(`Participant ${participant.identity} disconnected.`);
+    document.getElementById(`parent-${participant.sid}`).remove();
+  }
+
+  trackSubscribed(identity, track) {
+    document.getElementById(identity).appendChild(track.attach());
+  }
+  trackUnsubscribed(track) {
+    track.detach().forEach(element => element.remove());
   }
 }
